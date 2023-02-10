7d0cb780849aaa93f35194fe3d3c901f022ea20f
nkr413 <nkgithubnarkuziev108@gmail.com>
2023-02-11 02:47:16 +0500

    add: new data to base

--- src/content.js
+++ src/content.js
@@ -1,172 +1,387 @@
-let base = [
+const base = [
 	{
 		"name" : "PageSpeed Insights",
-		"url" : "https://pagespeed.web.dev/",
+		"url" : {
+			"website" : "https://pagespeed.web.dev/",
+		},
 		"description" : "Find out how to speed up the loading of your pages on any devices",
-		"type" : ["website", "analyze", "speed", "tool", "page"],
+		"type" : ["analyze", "speed", "tool"],
 		"id" : "0"
 	},
 	{
 		"name" : "CSS Gradient",
-		"url" : "https://cssgradient.io/",
+		"url" : {
+			"website" : "https://cssgradient.io/",
+		},
 		"description" : "CSS Gradient is a happy little website and free tool that lets you create a gradient background for websites",
-		"type" : ["css", "gradient", "background", "tool", "page"],
+		"type" : ["css", "gradient", "background", "design"],
 		"id" : "0"
 	},
 	{
 		"name" : "Unicode Character Table",
-		"url" : "https://unicode-table.com/en/",
+		"url" : {
+			"website" : "https://unicode-table.com/en/",
+		},
 		"description" : "Website with Unicode characters",
 		"type" : ["unicode", "emoji", "character", "symbol"],
 		"id" : "0"
 	},
 	{
 		"name" : "Google Fonts",
-		"url" : "hhhttps://fonts.google.com/",
+		"url" : {
+			"website" : "https://fonts.google.com/",
+		},
 		"description" : "A service where a large number of different fonts are collected",
 		"type" : ["fonts", "collection", "character", "symbol"],
 		"id" : "0"
 	},
 	{
 		"name" : "Code IMG",
-		"url" : "https://codeimg.io/",
+		"url" : {
+			"website" : "https://codeimg.io/",
+		},
 		"description" : "It will make a beautiful picture out of your code fragment",
 		"type" : ["code", "tool", "design", "programming"],
 		"id" : "0"
 	},
 	{
 		"name" : "Glassmorphism CSS Generator",
-		"url" : "https://hype4.academy/tools/glassmorphism-generator",
+		"url" : {
+			"website" : "https://hype4.academy/tools/glassmorphism-generator",
+		},
 		"description" : "Flexible generation of beautiful CSS glassmorphism",
-		"type" : ["tool", "design", "generator", "background", "css"],
+		"type" : ["design", "generator", "background", "css"],
 		"id" : "0"
 	},
 	{
 		"name" : "JSON Parser",
-		"url" : "http://json.parser.online.fr/",
+		"url" : {
+			"website" : "http://json.parser.online.fr/",
+		},
 		"description" : "Website for parsing json files",
 		"type" : ["tool", "parser", "character", "file", "programming"],
 		"id" : "0"
 	},
 	{
 		"name" : "Carbon",
-		"url" : "hhttps://carbon.now.sh/",
+		"url" : {
+			"website" : "https://carbon.now.sh/",
+			"github" : "https://github.com/carbon-app/carbon"
+		},
 		"description" : "Create and share beautiful images of your source code",
 		"type" : ["code", "tool", "design", "programming"],
 		"id" : "0"
 	},
 	{
 		"name" : "Hyper",
-		"url" : "https://hyper.is/",
+		"url" : {
+			"website" : "https://hyper.is/",
+		},
 		"description" : "Project is to create a beautiful and extensible experience for command-line interface users, built on open web standards",
-		"type" : ["cmd", "command-line", "terminal", "tool", "programming"],
+		"type" : ["cmd", "command-line", "terminal", "tool", "programming", "windows", "macos", "linux"],
 		"id" : "0"
 	},
 	{
 		"name" : "Modern JavaScript Tutorial",
-		"url" : "https://javascript.info/",
+		"url" : {
+			"website" : "https://javascript.info/",
+		},
 		"description" : "Here is a tutorial on JavaScript, starting with the basics, which includes many subtleties and features of JavaScript/DOM",
 		"type" : ["tutorial", "js", "javascript", "documentation", "programming"],
 		"id" : "0"
 	},
 	{
 		"name" : "Namelix",
-		"url" : "https://namelix.com/",
+		"url" : {
+			"website" : "https://namelix.com/",
+		},
 		"description" : "Generate a short, brandable business name using artificial intelligence",
-		"type" : ["tool", "generator", "page", "website"],
+		"type" : ["tool", "generator"],
 		"id" : "0"
 	},
 	{
 		"name" : "Monkeytype",
-		"url" : "https://monkeytype.com/",
+		"url" : {
+			"website" : "https://monkeytype.com/",
+		},
 		"description" : "Analyzes the speed of your typing on the keyboard",
 		"type" : ["tool", "analyze", "speed"],
 		"id" : "0"
 	},
 	{
 		"name" : "Fluent Design",
-		"url" : "https://www.microsoft.com/design/fluent/#/",
+		"url" : {
+			"website" : "https://www.microsoft.com/design/fluent/#/",
+		},
 		"description" : "Fluent is an open-source, cross-platform design system that gives designers and developers the frameworks they need to create engaging product experiences-accessibility, internationalization, and performance included",
-		"type" : ["website", "design", "page", "tutorial"],
+		"type" : ["design", "tutorial"],
 		"id" : "0"
 	},
 	{
 		"name" : "Helix",
-		"url" : "https://helix-editor.com/",
+		"url" : {
+			"website" : "https://helix-editor.com/",
+		},
 		"description" : "A post-modern text editor",
-		"type" : ["tool", "code-editor", "text-editor", "programming"],
+		"type" : ["code-editor", "text-editor", "programming", "windows", "macos", "linux"],
 		"id" : "0"
 	},
 	{
 		"name" : "Kono Store",
-		"url" : "https://kono.store/",
+		"url" : {
+			"website" : "https://kono.store/",
+		},
 		"description" : "Online keyboard store",
-		"type" : ["website", "page", "store", "shop"],
+		"type" : ["store", "shop"],
 		"id" : "0"
 	},
 	{
 		"name" : "Left",
-		"url" : "https://100r.co/site/left.html",
+		"url" : {
+			"website" : "https://100r.co/site/left.html",
+			"itch.io" : "https://hundredrabbits.itch.io/left",
+		},
 		"description" : "Left is graphical plain-text editor written in Uxntal. Left doesn't do much, but packs many useful features in a tiny 13kb rom",
-		"type" : ["tool", "text-editor"],
+		"type" : ["tool", "text-editor", "windows", "macos", "linux"],
 		"id" : "0"
 	},
 	{
 		"name" : "Zen",
-		"url" : "https://zen.unit.ms/",
+		"url" : {
+			"website" : "https://zen.unit.ms/",
+		},
 		"description" : "A functional online text editor with a minimalistic design",
-		"type" : ["tool", "text-editor", "website", "markdown"],
+		"type" : ["tool", "text-editor", "markdown"],
 		"id" : "0"
 	},
 	{
 		"name" : "Orca",
-		"url" : "https://100r.co/site/orca.html",
+		"url" : {
+			"website" : "https://100r.co/site/orca.html",
+			"itch.io" : "https://hundredrabbits.itch.io/orca",
+		},
 		"description" : "Orca is a two-dimensional esoteric programming language in which every letter of the alphabet is an operator, where lowercase letters operate on bang, uppercase letters operate each frame",
-		"type" : ["tool", "code", "programming"],
+		"type" : ["code", "programming", "windows", "macos", "linux"],
 		"id" : "0"
 	},
 	{
 		"name" : "OpenNetMeter",
-		"url" : "https://github.com/Ashfaaq18/OpenNetMeter",
+		"url" : {
+			"github" : "https://github.com/Ashfaaq18/OpenNetMeter",
+		},
 		"description" : "A simple program to monitor your network/data usage. Made for the average windows user",
 		"type" : ["windows", "monitor", "network", "internet", "analyze", "tool"],
 		"id" : "0"
 	},
 	{
 		"name" : "DevToys",
-		"url" : "https://devtoys.app/",
+		"url" : {
+			"website" : "https://devtoys.app/",
+			"github" : "https://github.com/veler/DevToys",
+			"microsoft-store" : "https://apps.microsoft.com/store/detail/devtoys/9PGCV4V3BK4W?hl=uz-latn-uz&gl=uz&rtc=1",
+			"chocolatey" : "https://community.chocolatey.org/packages/devtoys/",
+		},
 		"description" : "An offline Windows app that helps developers in daily tasks",
 		"type" : ["windows", "tool", "programming", "code", "markdown"],
 		"id" : "0"
 	},
 	{
 		"name" : "Notally",
-		"url" : "https://play.google.com/store/apps/details?id=com.omgodse.notally",
+		"url" : {
+			"github" : "https://github.com/OmGodse/Notally",
+			"google-play" : "https://play.google.com/store/apps/details?id=com.omgodse.notally",
+			"f-droid" : "https://f-droid.org/packages/com.omgodse.notally/",
+		},
 		"description" : "A beautiful notes app",
-		"type" : ["android", "tool", "notes", "text-editor", "app"],
+		"type" : ["android", "notes", "text-editor"],
 		"id" : "0"
 	},
 	{
 		"name" : "Poet",
-		"url" : "https://poet.so/",
+		"url" : {
+			"website" : "https://poet.so/",
+		},
 		"description" : "Create beautiful images of Twitter posts",
-		"type" : ["tool", "website", "design"],
+		"type" : ["tool", "design"],
 		"id" : "0"
 	},
 	{
 		"name" : "Lite",
-		"url" : "https://github.com/rxi/lite",
+		"url" : {
+			"github" : "https://github.com/rxi/lite",
+		},
 		"description" : "Lite is a lightweight text editor written mostly in Lua — it aims to provide something practical, pretty, small and fast, implemented as simply as possible; easy to modify and extend, or to use without doing either",
-		"type" : ["tool", "code-editor", "code", "programming"],
+		"type" : ["tool", "code-editor", "code", "programming", "windows", "linux"],
 		"id" : "0"
 	},
-		{
+	{
 		"name" : "Purelist: Minimal To-Do List",
-		"url" : "https://apps.apple.com/us/app/purelist/id1549367431?platform=iphone",
+		"url" : {
+			"website" : "https://purelist.com/",
+			"appstore" : "https://apps.apple.com/app/id1549367431?ref=purelist",
+		},
 		"description" : "The most intuitive and easy to use list app for you. All your Tasks, Notes, and Reminders can be done with this app alone. Simple and minimal interface, no need to learn how to use it",
-		"type" : ["app", "to-do", "ios"],
+		"type" : ["to-do", "ios", "macos"],
 		"id" : "0"
-	}
+	},
+	{
+		"name" : "StorySet",
+		"url" : {
+			"website" : "https://storyset.com/",
+		},
+		"description" : "Awesome free customizable illustrations for your next project",
+		"type" : ["design"],
+		"id" : "0"
+	},
+	{
+		"name" : "TransferFile.io",
+		"url" : {
+			"website" : "https://www.transferfile.io/#/",
+		},
+		"description" : "Decentralized filesharing over the IPFS network",
+		"type" : ["tool", "network", "internet", "file"],
+		"id" : "0"
+	},
+	{
+		"name" : "Rainy Mood",
+		"url" : {
+			"website" : "https://rainymood.com/",
+			"google-play" : "https://play.google.com/store/apps/details?id=com.TailoredMusic.RainyMood",
+			"appstore" : "https://apps.apple.com/us/app/rainy-mood/id566752651",
+		},
+		"description" : "Rain Sounds for Sleep & Study",
+		"type" : ["sound", "study", "android", "ios"],
+		"id" : "0"
+	},
+	{
+		"name" : "PlayCode",
+		"url" : {
+			"website" : "https://playcode.io/",
+		},
+		"description" : "Javascript Playground",
+		"type" : ["programming", "study", "code"],
+		"id" : "0"
+	},
+	{
+		"name" : "Unit Notes",
+		"url" : {
+			"website" : "https://unit.app/",
+			"appstore" : "https://apps.apple.com/us/app/unit-notes-lists-tasks/id1422480663?platform=iphone"
+		},
+		"description" : "Redefining note-taking software",
+		"type" : ["tool", "ios", "to-do", "notes"],
+		"id" : "0"
+	},
+	{
+		"name" : "DevUtils",
+		"url" : {
+			"website" : "https://devutils.com/",
+			"appstore" : "https://apps.apple.com/us/app/id1533756032"
+		},
+		"description" : "All-in-one Toolbox for Developers",
+		"type" : ["tool", "macos", "programming", "code", "markdown"],
+		"id" : "0"
+	},
+	{
+		"name" : "QR & Barcode Scanner",
+		"url" : {
+			"github" : "https://github.com/wewewe718/QrAndBarcodeScanner",
+			"f-droid" : "https://f-droid.org/en/packages/com.example.barcodescanner/",
+			"app-gallary" : "https://appgallery.huawei.com/#/app/C102717909"
+		},
+		"description" : "An Android app for scanning QR codes and barcodes",
+		"type" : ["tool", "android", "qrcode", "barcode"],
+		"id" : "0"
+	},
+	{
+		"name" : "Fig",
+		"url" : {
+			"website" : "https://fig.io/",
+			"github" : "https://github.com/withfig",
+		},
+		"description" : "The next-generation command line",
+		"type" : ["tool", "windows", "macos", "linux", "programming", "cmd", "command-line", "terminal"],
+		"id" : "0"
+	},
+	{
+		"name" : "Bionic Reading",
+		"url" : {
+			"website" : "https://bionic-reading.com/",
+			"webapp" : "https://app.bionic-reading.com/",
+			"appstore" : "https://apps.apple.com/app/id1644658030",
+			"google-play" : "https://play.google.com/store/apps/details?id=com.bionic_reading_app",
+		},
+		"description" : "Faster, better, more focused, reading",
+		"type" : ["tool", "android", "ios"],
+		"id" : "0"
+	},
+	{
+		"name" : "InnerTune",
+		"url" : {
+			"github" : "https://github.com/z-huang/InnerTune",
+			"f-droid" : "https://f-droid.org/packages/com.zionhuang.music/",
+		},
+		"description" : "A Material 3 YouTube Music client for Android",
+		"type" : ["android", "music-player", "sound"],
+		"id" : "0"
+	},
+	{
+		"name" : "Clowd",
+		"url" : {
+			"github" : "https://github.com/clowd/Clowd"
+		},
+		"description" : "Simple but powerful screenshot and screen recording tool for Windows",
+		"type" : ["tool", "windows", "screen-recorder", "recorder"],
+		"id" : "0"
+	},
+	{
+		"name" : "Wireshark",
+		"url" : {
+			"website" : "https://www.wireshark.org/",
+			"gitlab" : "https://gitlab.com/wireshark/wireshark/-/tree/master",
+		},
+		"description" : "The world’s foremost and widely-used network protocol analyzer",
+		"type" : ["tool", "windows", "macos", "analyze", "network", "internet", "monitor"],
+		"id" : "0"
+	},
+	{
+		"name" : "Harmonoid",
+		"url" : {
+			"website" : "https://harmonoid.com/",
+			"google-play" : "https://play.google.com/store/apps/details?id=com.alexmercerind.harmonoid&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1",
+			"github" : "https://github.com/harmonoid/harmonoid",
+		},
+		"description" : "Plays & manages your music library. Looks beautiful & juicy",
+		"type" : ["windows", "linux", "android", "music-player", "sound"],
+		"id" : "0"
+	},
+	{
+		"name" : "Headlines",
+		"url" : {
+			"github" : "https://github.com/creativcoder/headlines",
+		},
+		"description" : "A simple news reading GUI app built in Rust",
+		"type" : ["windows", "linux", "macos", "news"],
+		"id" : "0"
+	},
+	{
+		"name" : "Wavosaur",
+		"url" : {
+			"website" : "https://www.wavosaur.com/",
+		},
+		"description" : "Cool free sound editor, audio editor, wav editor software for editing, processing and recording sounds, wav and mp3 files",
+		"type" : ["windows", "sound", "tool"],
+		"id" : "0"
+	},
+	{
+		"name" : "Android Distribution Chart",
+		"url" : {
+			"website" : "https://composables.co/tools/distribution-chart",
+		},
+		"description" : "This page contains statistics about the distribution of Android versions. All statistic data comes from Google's servers",
+		"type" : ["analyze", "android"],
+		"id" : "0"
+	},
 ];
 
 let shuffle = (array) => {
