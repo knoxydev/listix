2e1c1f13e55665707076aadcc440d4347afa5ea9
knoxydev <nkgithubnarkuziev108@gmail.com>
2024-05-25 15:33:33 +0500

    update: big changes

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 let inpWords = [];
 let tags = [];
+let page_count = 0;
+const click_counts = {};
 
-window.onload = () => {
+
+window.onload = () =>
+{
 	randomArrPlcHolder();
-	randomPlaceholderTxt();
 	createContent();
+	//create_content_new();
 	assemble_tags(base);
+  btn_event();
 
 	document.getElementById("apps-quantity").innerHTML = `Apps: ${base.length}`;
 	document.getElementById("tags-quantity").innerHTML = `Tags: ${tags.length}`;
 }
 
-function randomPlaceholderTxt() { document.getElementById("main-search-input").placeholder = `Enter for example: ${inpWords[Math.floor(Math.random() * inpWords.length)]}`; }
 
-function randomArrPlcHolder() {
+function dark(item) {
+  item.style.border = "2px solid var(--medium-dark-purple)";
+  item.style.backgroundColor = "var(--medium-dark-purple)";
+  item.style.color = "var(--light-purple)";
+}
+function light(item) {
+  item.style.border = "2px solid var(--lightest-purple)";
+  item.style.backgroundColor = "var(--lightest-purple)";
+  item.style.color = "var(--medium-dark-purple)";
+}
+function set_theme(item, int) {
+  const darktheme = window.matchMedia("(prefers-color-scheme: dark)");
+  if (int == 1) (darktheme.matches) ? dark(item) : light(item);
+  else if (int == 2) (darktheme.matches) ? light(item) : dark(item);
+}
+
+
+function randomArrPlcHolder()
+{
 	let arr = [];
 	let copyArrElem = (y) => {for (let x of y) arr.push(x);}
 	for (let x of base) copyArrElem(x['type']);
@@ -21,33 +43,37 @@ function randomArrPlcHolder() {
 	inpWords = arr.filter((item, pos) => arr.indexOf(item) == pos);
 }
 
-function assemble_tags(x) {
+
+function assemble_tags(x)
+{
 	const test = [];
 
 	base.forEach((item) => { for (const value of item['type']) test.push(value); });
 	tags = [...new Set(test)];
 	tags.sort();
-	
-	for (const i of tags) {
-  	let tagDiv = document.createElement('div');
-  	tagDiv.innerHTML = i;
-  	document.getElementById("tags-block").append(tagDiv);
+
+	for (const i of tags)
+  {
+  	let tag_btn = document.createElement('button');
+  	tag_btn.innerHTML = i;
+  	document.getElementById("tags-block").append(tag_btn);
 	}
 }
 
-function createContent() {
+
+function createContent()
+{
 	document.getElementById("main-content-block").innerHTML = "";
 
 	const createList = (xItem) =>
 	{
 		let mainUl = document.createElement('ul');
 
-		for (let item of xItem['type'])
-		{
+		for (let item of xItem['type']) {
 			mainUl.append((function() {
 				let mainLi = document.createElement('li');
 				mainLi.innerHTML = item;
-				return mainLi
+				return mainLi;
 			})());
 		}
 
@@ -60,7 +86,8 @@ function createContent() {
 		linksDiv.setAttribute("id", "app-links");
 		const keys = Object.entries(x["url"]);
 
-		for (let item of keys) {
+		for (let item of keys)
+		{
 			let link = document.createElement("a");
 			link.setAttribute("href", item[1]);
 			link.innerHTML = item[0];
@@ -70,10 +97,11 @@ function createContent() {
 		return linksDiv;
 	};
 
-	for (let item of base) {
+	for (let item of base)
+	{
 		let mainDiv = document.createElement('div');
 		let mainTitle = document.createElement('div');
-		let mainDes = document.createElement('p'); 
+		let mainDes = document.createElement('p');
 
 		mainDiv.setAttribute("id", `post-${item['id']}`);
 		mainTitle.innerHTML = item['name'];
@@ -87,69 +115,147 @@ function createContent() {
 		mainDiv.append(get_links(item));
 		mainDiv.append(document.createElement('hr'));
 		mainDiv.append(list);
-
 		document.getElementById("main-content-block").append(mainDiv);
 	}
 }
 
-function filterContent() {
-	let inpText = document.getElementById("main-search-input").value.toLowerCase().trim();
 
-	for (const i of document.querySelectorAll("#tags-block > div")) {
-		i.style.backgroundColor = "var(--light-purple)";
-		i.style.color = "var(--medium-dark-purple)";
-		i.style.border = "2px solid var(--light-purple)";
-		i.style = "#tags-block > div:hover { border: 2px solid var(--medium-dark-purple); background-color: var(--medium-dark-purple); color: white; }";
-	}
-
-	if (inpText == "") return createContent();
-
+function filter_content(btn_txt)
+{
 	let postsHave = [];
 	let postsHavent = [];
 
-	for (let item of base) {
-		if (item['type'].includes(inpText) == true) postsHave.push(`post-${item['id']}`);
+	for (let item of base)
+	{
+		if (item['type'].includes(btn_txt) == true) postsHave.push(`post-${item['id']}`);
 		else postsHavent.push(`post-${item['id']}`);
 	}
 
-	for (let item of postsHave) {
+	for (let item of postsHave)
+	{
 		let x = document.getElementById(item);
 		document.getElementById(item).style.display = "block";
 
-		for (let item of x.children[4].children) {
-			if (inpText == item.innerHTML) {
-				item.style.backgroundColor = "var(--light-purple)";
-				item.style.color = "var(--medium-dark-purple)";
-				item.style.border = "2px solid var(--light-purple)";
-			}
+		for (let item of x.children[4].children)
+		{
+			if (btn_txt == item.innerHTML) set_theme(item, 2);
+      else set_theme(item, 1);
 		}
 	}
 	for (let item of postsHavent) document.getElementById(item).style.display = "none";
-
-
-	const filter_tags = () => {
-		for (const i of document.querySelectorAll("#tags-block > div"))
-		{
-  		if (i.innerHTML == inpText) {
-  			i.style.backgroundColor = "var(--light-purple)";
-				i.style.color = "var(--medium-dark-purple)";
-				i.style.border = "2px solid var(--light-purple)";
-  		} else {
-  			i.style.backgroundColor = "var(--light-purple)";
-				i.style.color = "var(--medium-dark-purple)";
-				i.style.border = "2px solid var(--light-purple)";
-				i.style = "#tags-block > div:hover { border: 2px solid var(--medium-dark-purple); background-color: var(--medium-dark-purple); color: white; }";
-  		}
-		}
-	}
-	filter_tags();
+  prev_btn = btn_txt;
 }
 
-document.getElementById("main-search-input").addEventListener("input", (e) =>
-{
-	let inp = document.getElementById("main-search-input").value.toLowerCase();
-	document.getElementById("main-search-input").value = inp.replace(/\s/g, "-");
 
-	filterContent();
+function create_content_new()
+{
+	const get_tags = (x) =>
+	{
+		let main_ul = document.createElement('ul');
+
+		for (let item of x['type']) {
+			main_ul.append((function() {
+				let main_li = document.createElement('li');
+				main_li.innerHTML = item;
+				return main_li
+			})());
+		}
+
+		return main_ul;
+	};
+
+	const get_links = (x) =>
+	{
+		let links_div = document.createElement("div");
+		links_div.setAttribute("id", "app-links");
+		const keys = Object.entries(x["url"]);
+
+		for (let i of keys)
+		{
+			let link = document.createElement("a");
+			link.setAttribute("href", i[1]);
+			link.innerHTML = i[0];
+			links_div.append(link);
+		}
+
+		return links_div;
+	};
+
+	const take_elm_in_range = () =>
+	{
+		let elms = [];
+		if ((page_count + 10) > base.length) elms = base.slice(page_count, base.length);
+		for (let i = page_count; i < page_count + 5; i++) elms.push(base[i]);
+		return elms;
+	}
+
+	const elms = take_elm_in_range();
+
+	for (let elm of elms)
+	{
+		let main_div = document.createElement('div');
+		let title = document.createElement('div');
+		let descr = document.createElement('p');
+		let links = document.createElement('div');
+
+		main_div.setAttribute("id", `post-${elm['id']}`);
+		title.innerHTML = elm['name'];
+		title.setAttribute("id", "app-title");
+		descr.innerHTML = elm['description'];
+
+		const tags_list = get_tags(elm);
+		const links_list = get_links(elm);
+
+		main_div.append(title);
+		main_div.append(descr);
+		main_div.append(links_list);
+		main_div.append(document.createElement('hr'));
+		main_div.append(tags_list);
+		document.getElementById("main-content-block").append(main_div);
+	}
+}
+
+
+document.getElementById("main-content-block").addEventListener("scroll", function() {
+  if (document.getElementById("main-content-block").scrollTop +
+      document.getElementById("main-content-block").clientHeight >=
+      document.getElementById("main-content-block").scrollHeight)
+  {
+    create_content();
+  }
 });
+
+
+const btn_event = () =>
+{
+  const change_color = (curr_box) =>
+  {
+    document.querySelectorAll("#tags-block > button").forEach(box =>
+    {
+      if (curr_box == box) set_theme(box, 2);
+      else set_theme(box, 1);
+    });
+
+  };
+
+
+  document.querySelectorAll("#tags-block > button").forEach((box, idx) => {
+    box.addEventListener('click', () =>
+    {
+      if (!click_counts[idx]) click_counts[idx] = 0;
+
+      click_counts[idx]++;
+
+      if (click_counts[idx] === 2)
+      {
+        click_counts[idx] = 0;
+        set_theme(box, 1);
+        return createContent();
+      }
+
+      change_color(box);
+      filter_content(box.innerHTML);
+    });
+  });
+};
 
